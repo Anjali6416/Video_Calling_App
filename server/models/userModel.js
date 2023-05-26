115f268ccb37e13191bdffb78d62797edfc8fcb7
Anjali <anjaliprajapati1772001@gmail.com>
2023-05-26 21:03:21 +0530

    Schema is Created

---
+++ server/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
